d1ffd5c82ce82246c0ea3485129c2e4a046033f6
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-11-22 18:16:13 +0500

    Replace angular-l10n with @angular/localize
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- webClient/src/app/app.module.ts
+++ webClient/src/app/app.module.ts
@@ -11,30 +11,14 @@
 */
 
 import { CommonModule } from '@angular/common';
-import { NgModule, Inject } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ZluxButtonModule, ZluxPopupManagerModule } from '@zlux/widgets';
 
 import { AppComponent } from './app.component';
 import {HelloService} from './services/hello.service';
-
-// import { L10nConfig, L10nLoader, TranslationModule, StorageStrategy, ProviderType } from 'angular-l10n';
-import { TranslationModule, L10nConfig, ISOCode, L10nLoader, LOCALE_CONFIG,
-  TRANSLATION_CONFIG, LocaleConfig, TranslationConfig } from 'angular-l10n';
-import { Angular2L10nConfig, Angular2InjectionTokens } from 'pluginlib/inject-resources';
 import { StorageService } from './services/storage.service';
-
-
-const l10nConfig: L10nConfig = {
-  translation: {
-      providers: [],
-      composedLanguage: [ISOCode.Language, ISOCode.Country],
-      caching: true,
-      missingValue: 'No key'
-  }
-};
-
 
 @NgModule({
   declarations: [
@@ -46,22 +30,12 @@ const l10nConfig: L10nConfig = {
     FormsModule,
     ZluxButtonModule,
     ZluxPopupManagerModule,
-    TranslationModule.forRoot(l10nConfig)
   ],
   providers: [HelloService, StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-  constructor(
-    private l10nLoader: L10nLoader,
-    @Inject(Angular2InjectionTokens.L10N_CONFIG) private l10nConfig: Angular2L10nConfig,
-    @Inject(LOCALE_CONFIG) private localeConfig: LocaleConfig,
-    @Inject(TRANSLATION_CONFIG) private translationConfig: TranslationConfig,
-
-  ) {
-    this.localeConfig.defaultLocale = this.l10nConfig.defaultLocale;
-    this.translationConfig.providers = this.l10nConfig.providers;
-    this.l10nLoader.load();
+  constructor() {
   }
 }
 
