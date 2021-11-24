135df6e673a208f05ed2a6e14e46c40092d679dc
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-11-24 15:29:15 +0500

    Code cleanup
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- webClient/src/app/app.module.ts
+++ webClient/src/app/app.module.ts
@@ -11,30 +11,13 @@
 */
 
 import { CommonModule } from '@angular/common';
-import { Inject, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ZluxButtonModule, ZluxPopupManagerModule } from '@zlux/widgets';
-import { L10nCache, L10nConfig, L10nLoader, L10nLocale, L10nTranslationModule, L10nTranslationService, L10N_CONFIG, L10N_LOCALE } from 'angular-l10n';
-import { Angular2InjectionTokens } from 'pluginlib/inject-resources';
+import { L10nCache, L10nTranslationModule, L10nTranslationService } from 'angular-l10n';
 import { AppComponent } from './app.component';
 import { HelloService } from './services/hello.service';
 import { StorageService } from './services/storage.service';
-
-
-
-
-
-export const l10nConfig: L10nConfig = {
-  format: 'language-region',
-  providers: [
-    { name: 'app', asset: 'my asset' },
-  ],
-  cache: true,
-  keySeparator: '.',
-  defaultLocale: { language: 'en-US' },
-  schema: [],
-};
-
 
 @NgModule({
   declarations: [
@@ -48,35 +31,17 @@ export const l10nConfig: L10nConfig = {
     ZluxPopupManagerModule,
     {
       ngModule: L10nTranslationModule,
-      providers: [ L10nCache, L10nTranslationService ]
+      providers: [ L10nCache, L10nTranslationService ] // New Cache and Translation Service
     }
-
   ],
   providers: [HelloService, StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor(
-    private l10nLoader: L10nLoader,
     private translation: L10nTranslationService,
-    @Inject(Angular2InjectionTokens.PLUGIN_DEFINITION) private pluginDefinition: ZLUX.ContainerPluginDefinition,
-    //@Inject(Angular2InjectionTokens.L10N_CONFIG) private l10nConfigOld: Angular2L10nConfig,
-    //@Inject(Angular2InjectionTokens.L10N_LOADER) private myl10nLoader: L10nTranslationLoader,
-    @Inject(L10N_CONFIG) private l10nConfig: L10nConfig,
-    @Inject(L10N_LOCALE) private l10nLocale: L10nLocale,
   ) {
-    this.pluginDefinition;
-    console.log(this.l10nLoader); //.init();
     this.translation.init();
-    // this.translation.loadTranslation([{
-    //   name: 'app', asset: '', options: { plugin: this.pluginDefinition.getBasePlugin() }
-    // }], this.l10nConfig.defaultLocale).then(() => {console.log('loaded')});
-    // this.l10nConfig.defaultLocale = this.l10nConfigOld.defaultLocale;
-    // this.translationConfig.providers = this.l10nConfig.providers;
-    console.log(`l10nConfig`, this.l10nConfig);
-
-    console.log(`l10nLocale`, this.l10nLocale);
-    //this.myl10nLoader.init();
   }
 }
 
