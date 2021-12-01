083855582e8a82cf48abc21e15fa20bd59bfe180
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-12-01 10:12:22 +0500

    Add build64.sh script
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- zssServer/build/build64.sh
+++ zssServer/build/build64.sh
@@ -8,10 +8,45 @@
 #
 #  Copyright Contributors to the Zowe Project.
 ################################################################################
-set -e
+export _C89_ACCEPTABLE_RC=0
+WORKDIR=$(dirname "$0")
+TMP="${WORKDIR}/tmp"
+mkdir "${TMP}" 2>/dev/null
+cd "${TMP}" || exit 8
 
-./build31.sh
-./build64.sh
+TARGET="../../../lib/storage64.so"
+TARGET_ABS="$(cd "$(dirname "${TARGET}")"; pwd)/$(basename "${TARGET}")"
+LIBDIR=$(dirname "${TARGET}")
+mkdir "${LIBDIR}" 2>/dev/null
+rm -f "${TARGET}"
+
+if [ -z "${ZSS}" ]; then
+  ZSS="../../../../../zss"
+fi
+COMMON="${ZSS}/deps/zowe-common-c"
+
+if ! c89 \
+  -D_XOPEN_SOURCE=600 \
+  -DAPF_AUTHORIZED=0 \
+  -DNOIBMHTTP \
+  "-Wa,goff" "-Wc,lp64,langlvl(EXTC99),float(HEX),agg,expo,list(),so(),search(),\
+  goff,xref,gonum,roconst,gonum,asm,asmlib('SYS1.MACLIB'),asmlib('CEE.SCEEMAC'),dll" \
+   -Wl,lp64,dll \
+  -I "${ZSS}/h" \
+  -I "${COMMON}/h" \
+  -o "${TARGET}" \
+  ../../src/storage.c \
+  ../pluginAPI64.x
+then
+  echo "${TARGET_ABS} - build failed"
+  RC=8
+else
+  echo "${TARGET_ABS} - build successful"
+  extattr +p "${TARGET}"
+  RC=0
+fi
+
+exit $RC
 ################################################################################
 #  This program and the accompanying materials are
 #  made available under the terms of the Eclipse Public License v2.0 which accompanies
